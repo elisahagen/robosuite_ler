e5ed3eeddb2b7a06ba6e8909ac2b9ddbcb72fdee
elisahagen <elisa.hagensieker@sereact.ai>
2025-11-12 10:32:13 +0000

    initial commit robosuite

---
+++ models/robots/manipulators/mounted_panda_robot.py
@@ -0,0 +1,5 @@
+from robosuite.models.robots.manipulators.panda_robot import Panda
+
+class MountedPanda(Panda):
+    """Mounted Panda variant for LIBERO tasks."""
+    pass
---
+++ models/robots/manipulators/on_the_ground_panda_robot.py
@@ -0,0 +1,7 @@
+from robosuite.models.robots.manipulators.panda_robot import Panda
+
+class OnTheGroundPanda(Panda):
+    """
+    LIBERO-compatible Panda variant (same XML as standard Panda).
+    """
+    pass
